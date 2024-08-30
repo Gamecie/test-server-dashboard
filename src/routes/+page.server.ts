1dd19edb729fdcad82987b8b14a1226b957cc326
Christofer Calvin <christofer.calvin@zicare.id>
2024-08-30 19:54:53 +0700

    fix +page.server.ts

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,9 +1,8 @@
 import { getServerLists } from "$db/zicare_tsd/server";
 
-export async function get(request: Request): Promise<{ data: JSON }> {   
+export async function load(request: Request): Promise<{ data: JSON }> {   
   // get employee from MongoDB
   const task = await getServerLists();
-  console.log({task});
   // return JSON response
-  return { data : task };
+  return { data : JSON.stringify(task) };
 }
